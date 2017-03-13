33634063301a899e3bcd7add3caaff609df11c85
TonyHag <to_hager@hotmail.com>
2017-03-13 15:21:19 +0100

    Lagt til Delete history funksjon

--- HistoryActivity.java
+++ HistoryActivity.java
@@ -1,12 +1,16 @@
 package com.example.tony.payday;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.tony.payday.logic.HistoryAdapter;
 import com.example.tony.payday.model.History;
@@ -24,9 +28,14 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-        list = getListFromPref();
+
         toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
         setSupportActionBar(toolbar);
+        initListView();
+
+    }
+    public void initListView(){
+        list = getListFromPref();
         hListView = (ListView) findViewById(R.id.historyListView);
 
 
@@ -34,7 +43,6 @@ public class HistoryActivity extends AppCompatActivity {
         hListView.setAdapter(hAdapter);
 
         hAdapter.notifyDataSetChanged();
-
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,5 +73,36 @@ public class HistoryActivity extends AppCompatActivity {
         }
 
         return hlist;
+    }
+    public void deleteHistory(){
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("list", "");
+        editor.apply();
+        Context context = getApplicationContext();
+        CharSequence text = "History Deleted";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        initListView();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
+        if (id == R.id.action_delete){
+            deleteHistory();
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
