7f60222414023b93bc22937709f3e1c9f9a09027
TonyHag <to_hager@hotmail.com>
2017-03-13 14:54:38 +0100

    Lagt til top tool bar på history activity

--- HistoryActivity.java
+++ HistoryActivity.java
@@ -4,6 +4,8 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.ListView;
 
 import com.example.tony.payday.logic.HistoryAdapter;
@@ -17,13 +19,14 @@ public class HistoryActivity extends AppCompatActivity {
     HistoryAdapter hAdapter;
     ArrayList<History> list;
     String stringlist;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
         list = getListFromPref();
-
-
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
         hListView = (ListView) findViewById(R.id.historyListView);
 
 
@@ -33,12 +36,19 @@ public class HistoryActivity extends AppCompatActivity {
         hAdapter.notifyDataSetChanged();
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_other, menu);
+        return true;
+    }
     public ArrayList<History> getListFromPref(){
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         stringlist = preferences.getString("list", "");
+        ArrayList<History> hlist = new ArrayList<History>();
         if (!stringlist.equals("")) {
             ArrayList<String> list1 = new ArrayList<String>(Arrays.asList(stringlist.split(",")));
-            ArrayList<History> hlist = new ArrayList<History>();
+
 
             for (String item : list1) {
                 String a; // name
@@ -51,9 +61,9 @@ public class HistoryActivity extends AppCompatActivity {
                 History h = new History(a, b, c);
                 hlist.add(h);
             }
-            return hlist;
+
         }
 
-        return null;
+        return hlist;
     }
 }
